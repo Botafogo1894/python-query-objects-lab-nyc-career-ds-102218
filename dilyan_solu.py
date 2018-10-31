b8b03221c847203906c1fa9ead5bebc6aecb834c
Botafogo1894 <pruchka@gmail.com>
2018-10-31 14:58:47 -0400

    first half done

--- dilyan_solu.py
+++ dilyan_solu.py
@@ -1,9 +1,12 @@
 from person import Person
 from query import Query
 
-Person("Jeff", 31)
-Person("Molly", 24)
-Person("Kevin", 38)
-Person("Rachel", 27)
-Person("Devin", 25)
-Person("Michelle", 33)
+jeff = Person("Jeff", 31)
+molly = Person("Molly", 24)
+kevin = Person("Kevin", 38)
+rachel = Person("Rachel", 27)
+devin = Person("Devin", 25)
+michelle = Person("Michelle", 33)
+jeff2 = Person("Jeff", 33)
+
+Query.find_by_name(Person, "Jeff")
--- query.py
+++ query.py
@@ -1,2 +1,34 @@
+
 class Query:
-    pass
+
+    @classmethod
+    def count(cls, other_cls):
+        count = 0
+        for item in other_cls.all():
+            count += 1
+        return count
+    @classmethod
+    def find_by_name(cls, other_cls, name):
+        for item in other_cls.all():
+            if item.name == name:
+                return item
+                break
+    @classmethod
+    def name_starts_with(cls, other_cls, letter):
+        empty = []
+        for item in other_cls.all():
+            if item.name.startswith(letter):
+                empty.append(item)
+        return empty
+    @classmethod
+    def is_older_than(cls, other_cls, number):
+        empty = []
+        for item in other_cls.all():
+            if item.age > number:
+                empty.append(item)
+        return empty
+
+    @classmethod
+    def mean_age(cls, other_cls):
+        all_ages = list(map(lambda item: item.age, other_cls.all()))
+        return sum(all_ages)/len(all_ages)
